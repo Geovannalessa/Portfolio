599ac62cab2f04419f5d3f2829acf4bf0916d178
Geovanna Lessa <geovannalessa2@gmail.com>
2025-04-12 10:06:12 -0300

    imagens do projeto mudam de acordo com o tema selecionado

--- script-padrao.js
+++ script-padrao.js
@@ -1,3 +1,14 @@
+function atualizarImagensTema() {
+    const modoEscuro = document.body.classList.contains('dark-mode');
+    const imagens = document.querySelectorAll('.imagem-projeto');
+  
+    imagens.forEach(img => {
+      const novaSrc = modoEscuro ? img.dataset.dark : img.dataset.light;
+      img.src = novaSrc;
+    });
+  }
+
 function mudaTema(btn){
     document.body.classList.toggle('dark-mode');
+    atualizarImagensTema();
 }
