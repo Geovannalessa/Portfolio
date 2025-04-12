6a8cf7b8a3237896c0c12cf49f4b77248e8bdebb
Geovanna Lessa <geovannalessa2@gmail.com>
2025-04-12 11:30:42 -0300

    Adicionando btn de tradução para o inglês

--- script-padrao.js
+++ script-padrao.js
@@ -1,3 +1,27 @@
+window.addEventListener("load", function() {
+    let idiomaAtual = 'pt';
+
+    const btn = document.getElementById('btn-idioma');
+    const elementos = document.querySelectorAll('.translate');
+  
+    function mudaIdioma() {
+      idiomaAtual = idiomaAtual === 'pt' ? 'en' : 'pt';
+  
+      elementos.forEach(el => {
+        el.textContent = el.dataset[idiomaAtual];
+      });
+  
+      btn.textContent =
+        idiomaAtual === 'pt' ? 'english' : 'português';
+    }
+  
+    btn.addEventListener('click', mudaIdioma);
+});
+
+
+
+
+
 function atualizarImagensTema() {
     const modoEscuro = document.body.classList.contains('dark-mode');
     const imagens = document.querySelectorAll('.imagem-projeto');
@@ -12,3 +36,6 @@ function mudaTema(btn){
     document.body.classList.toggle('dark-mode');
     atualizarImagensTema();
 }
+
+let idiomaAtual = 'pt';
+
